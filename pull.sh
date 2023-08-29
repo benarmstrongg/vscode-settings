30c17d6d216f6611b35112b6566497a7f4dc28b9
ben <benarmstrongg@gmail.com>
2023-08-29 11:49:10 -0400

    init

---
+++ pull.sh
@@ -0,0 +1,3 @@
+VSCODE_DIR=~/Library/Application\ Support/Code/User
+cp "$VSCODE_DIR"/settings.json .
+cp "$VSCODE_DIR"/keybindings.json .
---
+++ push.sh
@@ -0,0 +1,3 @@
+VSCODE_DIR=~/Library/Application\ Support/Code/User
+cp ./settings.json "$VSCODE_DIR"/settings.json
+cp ./keybindings.json "$VSCODE_DIR"/keybindings.json
